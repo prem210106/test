2e266e4383dede734459f289d29e5647dd592db5
f3-bilp <prem.21106patel@gmail.com>
2025-11-06 17:55:00 +0530

    home

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1> About page</h1>
+      <p>Welcome to [Your Company Name], a leading IT solutions provider committed to driving digital
+        transformation and innovation. We specialize in delivering cutting-edge technology solutions that empower
+        businesses to stay ahead in a rapidly evolving digital world.
+      </p>
+      <p>
+      Since our inception in 2022, we ve partnered with clients across industries to design, develop, and
+      implement reliable, scalable, and secure IT systems. Our team of skilled professionals brings together
+      expertise in software development, cloud computing, cybersecurity, IT consulting, and data analytics to
+      create solutions that truly make a difference.
+   </p>
+
+    <p>
+      At techStota, we believe technology should simplify, not complicate. Our mission is to help
+      businesses leverage the power of IT to enhance efficiency, improve customer experiences, and achieve
+      sustainable growth.
+    </p>
+    <p>
+      Our Core Values
+    </p>
+      
+      </div>
   )
 }
 
-export default About
+export default About;
